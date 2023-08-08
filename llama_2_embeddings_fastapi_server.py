a4f04f381ae0bf872e6041230b419c1fc0bb62f3
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-08 00:00:51 -0400

    Update llama_2_embeddings_fastapi_server.py

--- llama_2_embeddings_fastapi_server.py
+++ llama_2_embeddings_fastapi_server.py
@@ -6,22 +6,24 @@ import urllib.request
 import asyncio
 import shutil
 import subprocess
-import psutil
-from typing import List
+import traceback
+from typing import List, Optional
 from datetime import datetime
 import numpy as np
 from decouple import config
+import uvicorn
+import psutil
+import fastapi
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from langchain.embeddings import LlamaCppEmbeddings
 from pydantic import BaseModel
 from sklearn.metrics.pairwise import cosine_similarity
-from sqlalchemy import Column, String, Float, DateTime, Integer, UniqueConstraint, text
+from sqlalchemy import Column, String, Float, DateTime, Integer, UniqueConstraint
+from sqlalchemy import text as sql_text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError, OperationalError
-import fastapi
-import uvicorn
 
 # Note: the Ramdisk setup and teardown requires sudo; to enable password-less sudo, edit your sudoers file with `sudo visudo`.
 # Add the following lines, replacing username with your actual username
@@ -43,29 +45,39 @@ else:
     USE_SECURITY_TOKEN = False
 DATABASE_URL = "sqlite+aiosqlite:///embeddings.db"
 LLAMA_EMBEDDING_SERVER_LISTEN_PORT = config("LLAMA_EMBEDDING_SERVER_LISTEN_PORT", default=8089, cast=int)
+DEFAULT_MODEL_NAME = "llama2_7b_chat_uncensored"
 USE_RAMDISK = config("USE_RAMDISK", default=False, cast=bool)
 RAMDISK_SIZE_IN_GB = config("RAMDISK_SIZE_IN_GB", default=1, cast=int)
 RAMDISK_PATH = "/mnt/ramdisk"
 MAX_RETRIES = 3
 RETRY_DELAY_SECONDS = 1
 model_cache = {} # Model cache to store loaded models
+logger.info(f"USE_RAMDISK is set to: {USE_RAMDISK}")
+
 
 def setup_ramdisk():
-    # Check if there's enough available RAM
-    total_ram_gb = psutil.virtual_memory().total / (1024 ** 3)  # Total RAM in GB
+    total_ram_gb = psutil.virtual_memory().total / (1024 ** 3)  # Check total and available RAM; Total RAM in GB
     available_ram_gb = psutil.virtual_memory().available / (1024 ** 3)  # Available RAM in GB
+    ramdisk_size_gb = min(RAMDISK_SIZE_IN_GB, available_ram_gb - 2)  # Determine the appropriate RAM disk size; leave 2G of RAM free
+    ramdisk_size_mb = int(ramdisk_size_gb * 1024)  # Convert the size to MB
+    ramdisk_size_str = f"{ramdisk_size_mb}M"  # Format the size as a string with megabytes
     if RAMDISK_SIZE_IN_GB > total_ram_gb:
         raise ValueError(f"Cannot allocate {RAMDISK_SIZE_IN_GB}G for RAM Disk. Total system RAM is {total_ram_gb:.2f}G.")
-    elif RAMDISK_SIZE_IN_GB > available_ram_gb:
-        raise ValueError(f"Cannot allocate {RAMDISK_SIZE_IN_GB}G for RAM Disk. Only {available_ram_gb:.2f}G is available.")
     logger.info("Setting up RAM Disk...")
     os.makedirs(RAMDISK_PATH, exist_ok=True)
-    try:
-        subprocess.run(f"sudo mount -t tmpfs -o size={RAMDISK_SIZE_IN_GB}G tmpfs {RAMDISK_PATH}", shell=True, check=True)
-        logger.info(f"RAM Disk set up at {RAMDISK_PATH} with size {RAMDISK_SIZE_IN_GB}G")
-    except subprocess.CalledProcessError:
-        logger.error("Failed to set up RAM Disk.")
-        raise
+    try: # Try to set up the RAM Disk
+        subprocess.run(["sudo", "mount", "-t", "tmpfs", "-o", f"size={ramdisk_size_str}", "tmpfs", RAMDISK_PATH], check=True)
+        logger.info(f"RAM Disk set up at {RAMDISK_PATH} with size {ramdisk_size_gb}G")
+    except subprocess.CalledProcessError as e:
+        if os.path.ismount(RAMDISK_PATH): # If there's a failure and RAM Disk already exists, attempt to unmount and try again
+            logger.error(f"Failed to set up RAM Disk, but an existing RAM Disk was found. Attempting to unmount and try again...")
+            subprocess.run(["sudo", "umount", RAMDISK_PATH], check=False)
+            subprocess.run(["sudo", "mount", "-t", "tmpfs", "-o", f"size={ramdisk_size_str}", "tmpfs", RAMDISK_PATH], check=True)
+            logger.info(f"RAM Disk set up at {RAMDISK_PATH} with size {ramdisk_size_gb}G")
+        else:
+            logger.error(f"Failed to set up RAM Disk: {e}")
+            logger.error(f"Command output: {e.output}")
+            raise
 
 def clear_ramdisk():
     logger.info("Clearing RAM Disk...")
@@ -78,7 +90,7 @@ def clear_ramdisk():
 
 class EmbeddingRequest(BaseModel):
     text: str
-    model_name: str
+    model_name: str = DEFAULT_MODEL_NAME
 
 app = FastAPI(docs_url="/")  # Set the Swagger UI to root
 
@@ -116,7 +128,7 @@ class TextEmbedding(Base):
 class SimilarityRequest(BaseModel):
     text1: str
     text2: str
-    model_name: str
+    model_name: Optional[str] = DEFAULT_MODEL_NAME
 
 async def execute_with_retry(func, *args, **kwargs):
     retries = 0
@@ -135,8 +147,8 @@ async def execute_with_retry(func, *args, **kwargs):
 async def initialize_db():
     logger.info("Initializing database...")
     async with engine.begin() as conn:
-        await conn.execute(text("PRAGMA journal_mode=WAL;")) # Set SQLite to use Write-Ahead Logging (WAL) mode
-        await conn.execute(text("PRAGMA busy_timeout = 2000;")) # Increase the busy timeout (for example, to 2 seconds)
+        await conn.execute(sql_text("PRAGMA journal_mode=WAL;")) # Set SQLite to use Write-Ahead Logging (WAL) mode
+        await conn.execute(sql_text("PRAGMA busy_timeout = 2000;")) # Increase the busy timeout (for example, to 2 seconds)
         await conn.run_sync(Base.metadata.create_all)
     logger.info("Database initialization completed.")
 
@@ -145,8 +157,8 @@ def download_models():
         'https://huggingface.co/TheBloke/llama2_7b_chat_uncensored-GGML/resolve/main/llama2_7b_chat_uncensored.ggmlv3.q3_K_L.bin',
         'https://huggingface.co/TheBloke/WizardLM-1.0-Uncensored-Llama2-13B-GGML/resolve/main/wizardlm-1.0-uncensored-llama2-13b.ggmlv3.q3_K_L.bin'
     ]
-    downloaded_model_names = []
-    current_file_path = os.path.abspath(__file__) # Ensuring the directory is consistent regardless of how the module is used
+    model_names = [os.path.basename(url) for url in list_of_model_download_urls]
+    current_file_path = os.path.abspath(__file__)
     base_dir = os.path.dirname(current_file_path)
     models_dir = os.path.join(base_dir, 'models')
     logger.info("Checking models directory...")
@@ -156,29 +168,29 @@ def download_models():
     else:
         logger.info(f"Models directory exists: {models_dir}")
     logger.info("Starting model downloads...")
-    for url in list_of_model_download_urls:
-        filename = os.path.join(models_dir, os.path.basename(url))
-        model_name = os.path.splitext(os.path.basename(url))[0]
-        if not os.path.exists(filename): # Download the file only if it doesn't exist
-            logger.info(f"Downloading model {model_name} from {url}...")
+    for url, model_name_with_extension in zip(list_of_model_download_urls, model_names):
+        filename = os.path.join(models_dir, model_name_with_extension)
+        if not os.path.exists(filename):
+            logger.info(f"Downloading model {model_name_with_extension} from {url}...")
             urllib.request.urlretrieve(url, filename)
-            downloaded_model_names.append(model_name)
             logger.info(f"Downloaded: {filename}")
         else:
             logger.info(f"File already exists: {filename}")
-    if USE_RAMDISK: # If RAM Disk is enabled, copy models to RAM Disk only if not present
+    if USE_RAMDISK:
+        logger.info("RAM Disk is enabled. Checking RAM Disk for models...")
         ramdisk_models_dir = os.path.join(RAMDISK_PATH, 'models')
         os.makedirs(ramdisk_models_dir, exist_ok=True)
-        for model_name in downloaded_model_names:
-            filename = os.path.join(models_dir, f"{model_name}.bin")
-            ramdisk_filename = os.path.join(ramdisk_models_dir, f"{model_name}.bin")
+        for model_name_with_extension in model_names:
+            filename = os.path.join(models_dir, model_name_with_extension)
+            ramdisk_filename = os.path.join(ramdisk_models_dir, model_name_with_extension)
             if not os.path.exists(ramdisk_filename):
+                logger.info(f"Copying model {model_name_with_extension} to RAM Disk at {ramdisk_filename}...")
                 shutil.copyfile(filename, ramdisk_filename)
-                logger.info(f"Copied model {model_name} to RAM Disk at {ramdisk_filename}")
+                logger.info(f"Copied model {model_name_with_extension} to RAM Disk at {ramdisk_filename}")
             else:
-                logger.info(f"Model {model_name} already exists in RAM Disk at {ramdisk_filename}")
+                logger.info(f"Model {model_name_with_extension} already exists in RAM Disk at {ramdisk_filename}")
     logger.info("Model downloads completed.")
-    return downloaded_model_names
+    return model_names
 
 async def get_embedding_from_db(text, model_name):
     logger.info(f"Retrieving embedding for '{text}' using model '{model_name}' from database...")
@@ -187,31 +199,33 @@ async def get_embedding_from_db(text, model_name):
 async def _get_embedding_from_db(text, model_name):
     async with AsyncSessionLocal() as session:
         result = await session.execute(
-            "SELECT embedding_json FROM embeddings WHERE text=:text AND model_name=:model_name",
+            sql_text("SELECT embedding_json FROM embeddings WHERE text=:text AND model_name=:model_name"),
             {"text": text, "model_name": model_name},
         )
         row = result.fetchone()
         if row:
+            embedding_json = row[0]
+            logger.info(f"Embedding JSON retrieved from database: {embedding_json}")
             logger.info(f"Embedding found in database for '{text}' using model '{model_name}'")
-            return json.loads(row[0])
+            return json.loads(embedding_json)
         logger.info(f"Embedding not found in database for '{text}' using model '{model_name}'")
         return None
     
-async def get_or_compute_embedding(request: EmbeddingRequest, req: Request) -> dict:
+async def get_or_compute_embedding(request: EmbeddingRequest, req: Request = None) -> dict:
     request_time = datetime.utcnow()  # Capture request time as datetime object
     embedding_list = await get_embedding_from_db(request.text, request.model_name) # Check if embedding exists in the database
     if embedding_list is not None:
         return {"embedding": embedding_list}
     model = load_model(request.model_name)
-    embedding = calculate_sentence_embedding(model, request.text) # Compute the embedding if not in the database
-    if embedding is None:
+    embedding_list = calculate_sentence_embedding(model, request.text) # Compute the embedding if not in the database
+    if embedding_list is None:
         logger.error("Could not calculate the embedding for the given text")
         raise HTTPException(status_code=400, detail="Could not calculate the embedding for the given text")
-    embedding_list = embedding.tolist() # Serialize the numpy array to JSON and save to the database
-    embedding_json = json.dumps(embedding_list)
+    embedding_json = json.dumps(embedding_list) # Serialize the numpy array to JSON and save to the database
     response_time = datetime.utcnow()  # Capture response time as datetime object
     total_time = (response_time - request_time).total_seconds() # Calculate total time using datetime objects
-    await save_embedding_to_db(request.text, request.model_name, embedding_json, req.client.host, request_time, response_time, total_time)
+    client_host = req.client.host if req is not None else "localhost" # Handle the case when req is None
+    await save_embedding_to_db(request.text, request.model_name, embedding_json, client_host, request_time, response_time, total_time)
     return {"embedding": embedding_list}
 
 async def save_embedding_to_db(text, model_name, embedding_json, ip_address, request_time, response_time, total_time):
@@ -237,27 +251,32 @@ async def _save_embedding_to_db(text, model_name, embedding_json, ip_address, re
             logger.error(f"Error saving embedding to database: {e}")
             await session.rollback()
             raise
-
+        
 def load_model(model_name: str, raise_http_exception: bool = True):
-    models_dir = RAMDISK_PATH if USE_RAMDISK else os.path.join(BASE_DIRECTORY, 'models') # Determine the models_dir
     try:
-        if model_name in model_cache: # Check the cache first
+        logger.info(f"Attempting to load model: {model_name}")
+        models_dir = os.path.join(RAMDISK_PATH, 'models') if USE_RAMDISK else os.path.join(BASE_DIRECTORY, 'models')
+        logger.info(f"Searching in models directory: {models_dir}")
+        if model_name in model_cache:
             logger.info(f"Model {model_name} found in cache")
             return model_cache[model_name]
-        matching_files = glob.glob(os.path.join(models_dir, f"{model_name}*.bin")) # Find all files that match the model_name prefix
+        matching_files = glob.glob(os.path.join(models_dir, f"{model_name}*"))
         logger.info(f"Found {len(matching_files)} model files matching: {model_name}")
         if not matching_files:
             logger.error(f"No model file found matching: {model_name}")
             raise FileNotFoundError
-        matching_files.sort(key=os.path.getmtime, reverse=True) # Sort files by modification time (newest first)
-        model_file_path = matching_files[0] # Select the newest file
+        matching_files.sort(key=os.path.getmtime, reverse=True)
+        model_file_path = matching_files[0]
         logger.info(f"Loading model file: {model_file_path}")
         model_instance = LlamaCppEmbeddings(model_path=model_file_path)
         model_cache[model_name] = model_instance
         logger.info(f"Loaded model file: {model_file_path}")
         return model_instance
-    except FileNotFoundError:
-        logger.error(f"No model file found matching: {model_name}")
+    except TypeError as e:
+        logger.error(f"TypeError occurred while loading the model: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Exception occurred while loading the model: {e}")
         if raise_http_exception:
             raise HTTPException(status_code=404, detail="Model file not found")
         else:
@@ -268,12 +287,18 @@ def calculate_sentence_embedding(llama, text: str) -> np.array:
     retry_count = 0
     while sentence_embedding is None and retry_count < 3:
         try:
+            logger.info(f"Trying to calculate sentence embedding. Attempt: {retry_count + 1}")
             sentence_embedding = llama.embed_query(text)
+        except TypeError as e:
+            logger.error(f"TypeError in calculate_sentence_embedding: {e}")
+            raise
         except Exception as e:
             logger.error(f"Exception in calculate_sentence_embedding: {e}")
-            text = text[:-int(len(text) * 0.1)]  # Trim 10% of the text
+            text = text[:-int(len(text) * 0.1)]
             retry_count += 1
             logger.info(f"Trimming sentence due to too many tokens. New length: {len(text)}")
+    if sentence_embedding is None:
+        logger.error("Failed to calculate sentence embedding after multiple attempts")
     return sentence_embedding
 
 @app.exception_handler(SQLAlchemyError)
@@ -290,9 +315,11 @@ async def general_exception_handler(request: Request, exc: Exception):
 async def get_list_of_available_model_names(token: str = None):
     if USE_SECURITY_TOKEN and (token is None or token != SECURITY_TOKEN):
         raise HTTPException(status_code=403, detail="Unauthorized")
-    models_dir = RAMDISK_PATH if USE_RAMDISK else os.path.join(BASE_DIRECTORY, 'models') # Determine the models_dir
-    model_files = glob.glob(os.path.join(models_dir, "*.bin")) # Find all files with .bin extension
-    model_names = [os.path.splitext(os.path.basename(model_file))[0] for model_file in model_files]
+    models_dir = os.path.join(RAMDISK_PATH, 'models') if USE_RAMDISK else os.path.join(BASE_DIRECTORY, 'models')
+    logger.info(f"Looking for models in: {models_dir}") # Add this line for debugging
+    logger.info(f"Directory content: {os.listdir(models_dir)}") # Add this line for debugging
+    model_files = glob.glob(os.path.join(models_dir, "*.bin")) # Find all files with .ggmlv3.q3_K_L.bin extension
+    model_names = [os.path.splitext(os.path.splitext(os.path.basename(model_file))[0])[0] for model_file in model_files] # Remove both extensions
     return {"model_names": model_names}
 
 @app.post("/get_embedding_vector/", response_model=EmbeddingResponse)
@@ -303,16 +330,21 @@ async def get_embedding_vector(request: EmbeddingRequest, req: Request, token: s
         return await get_or_compute_embedding(request, req)
     except Exception as e:
         logger.error(f"An error occurred while processing the request: {e}")
+        logger.error(traceback.format_exc()) # Print the traceback
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.post("/compute_similarity_between_strings/", response_model=SimilarityResponse)
 async def compute_similarity_between_strings(request: SimilarityRequest, token: str = None):
+    logger.info(f"Received request: {request}")
     if USE_SECURITY_TOKEN and use_hardcoded_security_token and (token is None or token != SECURITY_TOKEN):
         raise HTTPException(status_code=403, detail="Unauthorized")
     try:
-        # Compute embeddings for both texts
-        embedding1_response = await get_embedding_vector(EmbeddingRequest(text=request.text1, model_name=request.model_name), Request(scope={"client": ("localhost", 0)}))
-        embedding2_response = await get_embedding_vector(EmbeddingRequest(text=request.text2, model_name=request.model_name), Request(scope={"client": ("localhost", 0)}))
+        # Create embedding requests for both texts
+        embedding_request1 = EmbeddingRequest(text=request.text1, model_name=request.model_name)
+        embedding_request2 = EmbeddingRequest(text=request.text2, model_name=request.model_name)
+        # Compute embeddings using the get_or_compute_embedding function
+        embedding1_response = await get_or_compute_embedding(embedding_request1, None)
+        embedding2_response = await get_or_compute_embedding(embedding_request2, None)
         # Extract the embedding vectors
         embedding1 = np.array(embedding1_response["embedding"])
         embedding2 = np.array(embedding2_response["embedding"])
@@ -324,12 +356,13 @@ async def compute_similarity_between_strings(request: SimilarityRequest, token: 
         return {
             "text1": request.text1,
             "text2": request.text2,
+            "similarity": similarity[0][0],
             "embedding1": embedding1.tolist(),
-            "embedding2": embedding2.tolist(),
-            "similarity": similarity[0][0]
+            "embedding2": embedding2.tolist()
         }
     except Exception as e:
         logger.error(f"An error occurred while processing the request: {e}")
+        traceback.print_exc() # Print the traceback to see where the error occurred
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.get("/", include_in_schema=False)
