46584dae74f62b94b864af1a085226fdd6246ed8
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-09 13:40:43 -0400

    Create setup_dockerized_app_on_fresh_machine.sh

---
+++ setup_dockerized_app_on_fresh_machine.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Update system packages
+echo "Updating system packages..."
+sudo apt-get update
+
+# Install Docker
+echo "Installing Docker..."
+sudo apt-get install docker.io
+
+# Start Docker service
+echo "Starting Docker service..."
+sudo systemctl start docker
+
+# Display Docker version
+echo "Checking Docker version..."
+sudo docker --version
+
+# Add the current user to the docker group
+echo "Adding current user to the Docker group..."
+sudo usermod -aG docker $USER
+
+# Remove the old directory if it exists
+echo "Removing old llama_embeddings_fastapi_service directory..."
+rm -rf llama_embeddings_fastapi_service
+
+# Clone the repository
+echo "Cloning the llama_embeddings_fastapi_service repository..."
+git clone https://github.com/Dicklesworthstone/llama_embeddings_fastapi_service
+
+# Change to the repository directory
+cd llama_embeddings_fastapi_service
+
+# Build the Docker image
+echo "Building the Docker image..."
+sudo docker build -t llama-embeddings .
+
+# Run the Docker container
+echo "Running the Docker container..."
+sudo docker run -p 8089:8089 llama-embeddings
+
+echo "Script completed!"
